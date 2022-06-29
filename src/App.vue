70d9cd0b781c12e80e18b44270ca85ea8ad2c809
Martin Corizzo <macorizzo@gmail.com>
2022-06-29 09:49:10 -0300

    terminado

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <HeaderComponent :pickedColor="pickedColor" />
+    <NavBar @isHard="isHard = $event" @squareQty="squareQty = $event" @colors="getColors" @pickedColor="getPicked"
+      :message="message" />
+    <ContainerComponent :squareQty="squareQty" :colors="colors" @selectedColor="getSelected" @pickedColor="getPicked"
+      @message="getMessage" :pickedColor="pickedColor" />
+    <square-component/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import NavBar from './components/NavBar.vue'
+import ContainerComponent from './components/ContainerComponent.vue'
+import SquareComponent from './components/SquareComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    NavBar,
+    ContainerComponent,
+    SquareComponent,
+  },
+  updated() {
+  },
+  data() {
+    return {
+      isHard: false,
+      squareQty: 0,
+      colors: [],
+      selectedColor: '',
+      pickedColor: '',
+      message: '',
+    }
+  },
+  methods: {
+    getColors(e) {
+      this.colors = e;
+    },
+    getSelected(e) {
+      this.selectedColor = e;
+    },
+    getPicked(e) {
+      this.pickedColor = e;
+    },
+    getMessage(e) {
+      this.message = e;
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  margin: 0;
+  all: unset;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #353535;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-family: "Montserrat", "Avenir";
+}
+</style>
---
+++ src/components/ContainerComponent.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <section class="container-component">
+    <div id="container">
+      <SquareComponent v-for="(square, index) in squareQty" :key="index" :color="colors[index]" :pickedColor="
+      pickedColor" @selectedColor="selectedColor = $event" />
+    </div>
+
+  </section>
+
+</template>
+
+<script lang="js">
+import SquareComponent from './SquareComponent.vue';
+
+export default {
+  name: 'container-component',
+  components: { SquareComponent },
+  props: ["squareQty", "colors", 'pickedColor'],
+  updated() {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  computed: {
+
+
+  }
+}
+
+
+</script>
+
+<style scoped lang="css">
+#container {
+  margin: 20px auto;
+  max-width: 601px;
+}
+
+</style>
